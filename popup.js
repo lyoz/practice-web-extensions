b1f4d7b7dddbc592528750e6bc7b8860e41b1924
lyoz <lyoz@users.noreply.github.com>
2019-09-02 12:46:07 +0900

    Introduce a User Interface

---
+++ popup.js
@@ -0,0 +1,5 @@
+const changeColor = document.getElementById('changeColor')
+chrome.storage.sync.get('color', data => {
+  changeColor.style.backgroundColor = data.color
+  changeColor.setAttribute('value', data.color)
+})
